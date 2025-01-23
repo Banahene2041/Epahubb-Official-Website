127c53e9b021a4b0fa8642f714ade90e9c186f27
Banahene2041 <0542342728c@gmail.com>
2025-01-23 10:55:43 +0000

    first commit

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import { HomeLayout } from "../components"
+import ColorPicker from "../components/HomeSections/ColorPicker"
+
+
+const HomePage = () => {
+
+  return (
+    <section>
+      <HomeLayout
+      />
+      <ColorPicker/>
+    </section>
+  )
+}
+export default HomePage
---
+++ src/utils/data.js
@@ -0,0 +1,147 @@
+import { assets } from "../assets/assets"
+
+export const links = [
+  {
+    id: 1,
+    name: "About Us",
+    subLinks: [
+      { name: "Why Epahubb", url: "#why-epahubb" },
+      { name: "Features", url: "#features" },
+      { name: "Integration", url: "#integration" },
+      { name: "How It Works", url: "#how-it-works" },
+      { name: "Best Solutions", url: "#best-solutions" },
+      { name: "Testimonials", url: "#testimonials" },
+      { name: "Faqs", url: "#faqs" },
+      { name: "Contact Us", url: "#contact-us" },
+    ],
+  },
+  { id: 2, name: "Features", subLinks: [] },
+  {
+    id: 3,
+    name: "Products",
+    subLinks: [
+      { name: "Kromonline", url: "https://kromonline.com" },
+      { name: "Teappos", url: "https://teappos.com" },
+      { name: "Teahrm", url: "https://teahrm.com" },
+      { name: "BuyData", url: "https://buydata.kromonline.com" },
+      { name: "Epasmart", url: "https://epasmart.epahubb.com/" },
+      { name: "Trufunder", url: "https://trufunder.io" },
+      { name: "Malosaonline", url: "https://malosaonline.com" },
+    ],
+  },
+  { id: 4, name: "FAQs", subLinks: [] },
+]
+
+export const customerReviews = [
+  {
+    name: "Elvis Justice",
+    company: "CEO. Trufunder Prop Firm LTD",
+    review:
+      "I had my first-ever prop firm web application developed by Epahubb Consult, and guess what? It was absolutely worth it!'",
+    image: `${assets.review_customer_one}`,
+  },
+  {
+    name: "Elvis Justice",
+    company: "CEO. Trufunder Prop Firm LTD",
+    review:
+      "I had my first-ever prop firm web application developed by Epahubb Consult, and guess what? It was absolutely worth it!'",
+    image: `${assets.review_customer_eight}`,
+  },
+  {
+    name: "Elvis Justice",
+    company: "CEO. Trufunder Prop Firm LTD",
+    review:
+      "I had my first-ever prop firm web application developed by Epahubb Consult, and guess what? It was absolutely worth it!'",
+    image: `${assets.review_customer_five}`,
+  },
+  {
+    name: "Elvis Justice",
+    company: "CEO. Trufunder Prop Firm LTD",
+    review:
+      "I had my first-ever prop firm web application developed by Epahubb Consult, and guess what? It was absolutely worth it!'",
+    image: `${assets.review_customer_two}`,
+  },
+  {
+    name: "Elvis Justice",
+    company: "CEO. Trufunder Prop Firm LTD",
+    review:
+      "I had my first-ever prop firm web application developed by Epahubb Consult, and guess what? It was absolutely worth it!'",
+    image: `${assets.review_customer_three}`,
+  },
+  {
+    name: "Elvis Justice",
+    company: "CEO. Trufunder Prop Firm LTD",
+    review:
+      "I had my first-ever prop firm web application developed by Epahubb Consult, and guess what? It was absolutely worth it!'",
+    image: `${assets.review_customer_four}`,
+  },
+  {
+    name: "Elvis Justice",
+    company: "CEO. Trufunder Prop Firm LTD",
+    review:
+      "I had my first-ever prop firm web application developed by Epahubb Consult, and guess what? It was absolutely worth it!'",
+    image: `${assets.review_customer_five}`,
+  },
+  {
+    name: "Elvis Justice",
+    company: "CEO. Trufunder Prop Firm LTD",
+    review:
+      "I had my first-ever prop firm web application developed by Epahubb Consult, and guess what? It was absolutely worth it!'",
+    image: `${assets.review_customer_six}`,
+  },
+  {
+    name: "Elvis Justice",
+    company: "CEO. Trufunder Prop Firm LTD",
+    review:
+      "I had my first-ever prop firm web application developed by Epahubb Consult, and guess what? It was absolutely worth it!'",
+    image: `${assets.review_customer_seven}`,
+  },
+  {
+    name: "Elvis Justice",
+    company: "CEO. Trufunder Prop Firm LTD",
+    review:
+      "I had my first-ever prop firm web application developed by Epahubb Consult, and guess what? It was absolutely worth it!'",
+    image: `${assets.review_customer_eight}`,
+  },
+  {
+    name: "Elvis Justice",
+    company: "CEO. Trufunder Prop Firm LTD",
+    review:
+      "I had my first-ever prop firm web application developed by Epahubb Consult, and guess what? It was absolutely worth it!'",
+    image: `${assets.review_customer_nine}`,
+  },
+  {
+    name: "Elvis Justice",
+    company: "CEO. Trufunder Prop Firm LTD",
+    review:
+      "I had my first-ever prop firm web application developed by Epahubb Consult, and guess what? It was absolutely worth it!'",
+    image: `${assets.review_customer_one}`,
+  },
+]
+
+export const faqs = [
+  {
+    id: 1,
+    question: "What service does Epahubb offer",
+    answer:
+      "Epahubb provides a wide range of tech solutions, including software development, web abd mobile app development, UI/UX design,e-commerce platforms, and IT consulting services",
+  },
+  {
+    id: 2,
+    question: "How can I get started with Epahubb",
+    answer:
+      "To get started with Epahubb Tech Consult, visit our website, identify your needs, contact our team, schedule a consultation, and collaborate to achieve tailored solutions for your business.",
+  },
+  {
+    id: 3,
+    question: "Who can benefits from Epahubb's services?",
+    answer:
+      "Epahubb's services benefits businesses of all sizes,startups, enterprise , and individuals seeking experts in software development,IT,consulting,digital transformation, tailored technology-driven strategies",
+  },
+  {
+    id: 4,
+    question: "How does Epahubb stay innovative in its approach?",
+    answer:
+      "Epahubb stays innovative by leveraging the latest technologies, prioritizing continuous learning, fostering a culture of creativity, and collaborating closely with clients to deliver tailored, forward-thinking solutions that meet evolving industry needs.",
+  },
+]
